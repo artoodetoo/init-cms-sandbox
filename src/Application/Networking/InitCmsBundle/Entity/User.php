c16c13615835d5cf7e8187b45d1cecebce6ecca7
artoodetoo <i.am@artoodetoo.org>
2014-01-31 10:59:07 +0600

    add vkontakte connect

--- src/Application/Networking/InitCmsBundle/Entity/User.php
+++ src/Application/Networking/InitCmsBundle/Entity/User.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the init_cms_sandbox package.
  *
- * (c) net working AG <info@networking.ch>
+ * (c) artoodetoo
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,8 +13,90 @@ namespace Application\Networking\InitCmsBundle\Entity;
 
 use Networking\InitCmsBundle\Entity\BaseUser;
 /**
- * @author Yorkie Chadwick <y.chadwick@networking.ch>
+ * @author artoodetoo
  */
-class User extends BaseUser{
+class User extends BaseUser
+{
+    /**
+     * @var string $vkontakteUid Identity from VK.COM
+     */
+    protected $vkontakteUid;
+    /**
+     * @var string $vkontakteName User name from VK.COM
+     */
+    protected $vkontakteName;
+    /**
+     * @var string $vkontakteData Extra data from VK.COM
+     */
+    protected $vkontakteData;
+
+    /**
+     * Set vkontakteUid
+     *
+     * @param string $vkontakteUid
+     * @return User
+     */
+    public function setVkontakteUid($vkontakteUid)
+    {
+        $this->vkontakteUid = $vkontakteUid;
+    
+        return $this;
+    }
+
+    /**
+     * Get vkontakteUid
+     *
+     * @return string 
+     */
+    public function getVkontakteUid()
+    {
+        return $this->vkontakteUid;
+    }
+
+    /**
+     * Set vkontakteName
+     *
+     * @param string $vkontakteName
+     * @return User
+     */
+    public function setVkontakteName($vkontakteName)
+    {
+        $this->vkontakteName = $vkontakteName;
+    
+        return $this;
+    }
+
+    /**
+     * Get vkontakteName
+     *
+     * @return string 
+     */
+    public function getVkontakteName()
+    {
+        return $this->vkontakteName;
+    }
+
+    /**
+     * Set vkontakteData
+     *
+     * @param json $vkontakteData
+     * @return User
+     */
+    public function setVkontakteData($vkontakteData)
+    {
+        $this->vkontakteData = $vkontakteData;
+    
+        return $this;
+    }
+
+    /**
+     * Get vkontakteData
+     *
+     * @return json 
+     */
+    public function getVkontakteData()
+    {
+        return $this->vkontakteData;
+    }
 
 }
